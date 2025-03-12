1e4041d06b153746c2566903da5c6f08518d4b38
duno1012 <alwaysnewandunique@gmail.com>
2025-03-12 09:45:49 +0900

    code

---
+++ ex20250312/ex20250312/Main.cpp
@@ -0,0 +1,27 @@
+#include <iostream> //include <stdio.h>
+using namespace std; //standard
+
+int main()
+{
+	cout << "Hello" << endl;
+	cout << 100 << " " << 3.33 << endl;
+
+	int a, b;
+	//scanf("%d %d", &a,&b);
+	cout << a << " " << b << endl;
+
+	//return 0; main에서만 return 0 스킵이 가능함, 엔트리 포인트. - 프로그램 종료
+
+}
+
+//#include <iostream> //include <stdio.h>
+//using namespace std; //standard
+//
+//int main()
+//{
+//	cout << "Hello" << endl;
+//	cout << 100 << " "<< 3.33 << endl;
+//
+//	//return 0; main에서만 return 0 스킵이 가능함, 엔트리 포인트. - 프로그램 종료
+//
+//}
